42212312f2375583ccab26799b52eaff561abde5
Amit kishore <kishoreamit5@gmail.com>
2020-06-25 17:43:57 +0530

    Updates

---
+++ precache-manifest.dad656cf55c1a07b5f92005904a1f3dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b38a3e6ef6562dbd501d077715f6104",
+    "url": "/calculator-ui/index.html"
+  },
+  {
+    "revision": "fe6690e4d98e04ba47f8",
+    "url": "/calculator-ui/static/css/main.a2eb5172.chunk.css"
+  },
+  {
+    "revision": "e61e51eacd7cf1f7cf9b",
+    "url": "/calculator-ui/static/js/2.dda6dfbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-ui/static/js/2.dda6dfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6690e4d98e04ba47f8",
+    "url": "/calculator-ui/static/js/main.bef9dc17.chunk.js"
+  },
+  {
+    "revision": "be0989bb0979636a6ae7",
+    "url": "/calculator-ui/static/js/runtime-main.11a6a6e2.js"
+  }
+]);
